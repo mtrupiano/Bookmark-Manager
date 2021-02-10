a67130e12d73cc4af1071310598053e6a5786cbc
mtrupiano <markt6749@gmail.com>
2021-02-10 08:44:37 -0800

    removed vestigial handlebars files

--- extension/popup.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener("load", () => {
-    chrome.windows.getCurrent({populate:true}, window => {
-        const activeTab = window.tabs.filter(tab => tab.active)[0];
-        console.log(activeTab);
-        document.getElementById("url_input").value = activeTab.url;
-        document.getElementById("name_input").value = activeTab.title;
-    })
-});
